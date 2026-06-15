77e3de48b8c24060f2462ecb239ac0186ac2549c
Nukri06 <munjishvilinukri16@gmail.com>
2026-06-15 20:15:08 +0400

    first commit

---
+++ scrollReset.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+          setTimeout(function() {
+              window.scrollTo(0, 0);
+          }, 10);
+      };
+
+    //   <!-- ანიმაცია გამიჯიუტდა index.htmlში და  იძულებული გავხდი გამომეყენებინა -->
